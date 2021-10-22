dc125e2fec572c544fb0730fbe207a70d8613573
kishore <91112126+kishorerio@users.noreply.github.com>
2021-10-22 11:24:52 +0530

    Add files via upload

--- Week 4/Coding Activity/4.1/task2.js
+++ Week 4/Coding Activity/4.1/task2.js
@@ -6,11 +6,16 @@
 
 function wordsToSentence(words) {
   //your code here
+  let words1 = "Hey";
+  let words2 = " There";
+  
+   let result = words1.concat(words2);
+   return result;
   
 }
 
 //console log results
-console.log('results', wordsToSentence(['hey', 'there']));
+console.log('results:', wordsToSentence(['hey', 'there']));
 
 //don't change this line
 if (typeof module !== 'undefined') {
